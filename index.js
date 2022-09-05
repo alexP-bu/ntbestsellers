84f79c2e8d087e5f09ce02f00a45d21524441df6
alexP-bu <apozin@bu.edu>
2022-09-05 16:16:46 -0400

    cleaned up code

--- index.js
+++ index.js
@@ -12,29 +12,19 @@ app.set('view engine', 'pug');
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', async (req, res) => {
-    const fic = [];
-    const non = [];
-    await getBooks(fic, API.URL_HARDCOVER_FICTION);
-    await getBooks(non, API.URL_HARDCOVER_NONFICTION);
+    const urls = [API.URL_HARDCOVER_FICTION, API.URL_HARDCOVER_NONFICTION];
+    const [fic, non] = await Promise.all(urls.map(getBooks));
     res.render('layout', {results: {fic: fic, non: non}});
 });
 
-const getBooks = async (array, url)  => {
-    await axios.get(url + "?api-key=" + API.Key)
-        .then( async res => {
-          const list = await res.data.results.books;
-          list.forEach( book => {
-            array.push(new Book(
-              book.primary_isbn13,
-              book.title,
-              book.author,
-              'https://www.bookshop.org/a/15886/' + book.primary_isbn13));
-          })
-        })
-        .catch(error => {
-          console.error(error);
-        });
-    return array;
-}
+const getBooks = async url  => {
+    const res = await axios.get(url + "?api-key=" + API.Key);
+    return res.data.results.books.map(book => new Book(              
+      book.primary_isbn13,
+      book.title,
+      book.author,
+      'https://www.bookshop.org/a/15886/' + book.primary_isbn13
+    ));
+};
 
 app.listen(8080);
