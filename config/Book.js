dc539068f3169f347c0cccd15eee72948a570604
alexP-bu <apozin@bu.edu>
2022-09-17 18:26:01 -0400

    just removed API.js as it contains key

---
+++ config/Book.js
@@ -0,0 +1,10 @@
+class Book {
+    constructor(isbn, title, author, link){
+        this.isbn = isbn;
+        this.title = title;
+        this.author = author;
+        this.link = link;
+    }
+}
+
+module.exports = Book;
